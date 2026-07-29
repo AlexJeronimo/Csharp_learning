4e6f381ca7196026178a43aa9ac491ff15d84125
Alex Jeronimo <jeronimoalex@gmail.com>
2026-07-29 16:28:44 +0300

    literals

--- Program.cs
+++ Program.cs
@@ -6,8 +6,18 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            Console.WriteLine("booooya");
-            Console.Read();
+
+            
+        }
+
+        static void Variables()
+        {
+            int x = -1;
+            int y;
+            y = 2;
+
+
+            Console.WriteLine("x = {0}, y = {1}", Convert.ToString(x), Convert.ToString(y));
         }
     }
 }
