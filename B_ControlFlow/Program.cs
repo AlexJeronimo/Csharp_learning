3e88981f0dc7c51e9d9fa80b4944035ff85d028d
Oleksandr Pidhurskyi <jeronimoalex@gmail.com>
2026-08-03 11:38:37 +0300

    home work 2

--- B_ControlFlow/Program.cs
+++ B_ControlFlow/Program.cs
@@ -1,45 +1,64 @@
-﻿Console.WriteLine("Eneter your age: ");
-int age = int.Parse(Console.ReadLine()!);
+﻿
 
-Console.WriteLine("Enetr your weight in kilograms: ");
-double weight = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Enetr your height in meters");
-double height = double.Parse(Console.ReadLine()!);
-double imt = weight / (height * height);
 
-bool isTooLow = imt <= 18.5;
-bool isNormal = imt > 18.5 && imt < 25;
-bool isAboveNormal = imt >= 25 && imt <= 30;
-bool isTooFat = imt > 30;
 
-bool isFat = isAboveNormal || isTooFat;
-
-if (isFat)
+static void ControlFlowIFELSE()
 {
-    Console.WriteLine("You'd better lose some weight");
-}
-else
-{
-    Console.WriteLine("Oh, you're in a good shape");
-}
+    Console.WriteLine("Eneter your age: ");
+    int age = int.Parse(Console.ReadLine()!);
 
-if (isFat || isTooFat)
-{
-    Console.WriteLine("Anyway it's time to get on diet");
-}
+    Console.WriteLine("Enetr your weight in kilograms: ");
+    double weight = double.Parse(Console.ReadLine()!);
+    Console.WriteLine("Enetr your height in meters");
+    double height = double.Parse(Console.ReadLine()!);
+    double imt = weight / (height * height);
 
-//ternary operation
-string description = age > 18 ? "You can drink alcohol" : "You should get a bit older";
-Console.WriteLine(description);
+    bool isTooLow = imt <= 18.5;
+    bool isNormal = imt > 18.5 && imt < 25;
+    bool isAboveNormal = imt >= 25 && imt <= 30;
+    bool isTooFat = imt > 30;
 
-//the same in if-else construction
-if (age > 18)
-{
-    description = "You can drink alcohol";
-}
-else
-{
-    description = "You should get a bit older";
-}
-Console.WriteLine(description);
+    bool isFat = isAboveNormal || isTooFat;
 
+    if (isFat)
+    {
+        Console.WriteLine("You'd better lose some weight");
+    }
+    else
+    {
+        Console.WriteLine("Oh, you're in a good shape");
+    }
+
+    if (isFat || isTooFat)
+    {
+        Console.WriteLine("Anyway it's time to get on diet");
+    }
+
+    //ternary operation
+    string description = age > 18 ? "You can drink alcohol" : "You should get a bit older";
+    Console.WriteLine(description);
+
+    //the same in if-else construction
+    if (age > 18)
+    {
+        description = "You can drink alcohol";
+    }
+    else
+    {
+        description = "You should get a bit older";
+    }
+    Console.WriteLine(description);
+
+
+}
+static void HW2Max()
+{
+    Console.WriteLine("Please neter 2 numbers:");
+    Console.WriteLine("Fist number");
+    int a = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Second number");
+    int b = int.Parse(Console.ReadLine()!);
+
+    string max = a > b ? $"A is max {a}" : $"B is max {b}";
+    Console.WriteLine(max);
+}
