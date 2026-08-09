b3fc885f98309db2cfcb7567352b2ea81fffdbae
Oleksandr Pidhurskyi <jeronimoalex@gmail.com>
2026-08-09 11:26:10 +0300

    params

--- D_OOP/Calculator.cs
+++ D_OOP/Calculator.cs
@@ -32,5 +32,15 @@ namespace D_OOP
             }
             return (double)sum / numbers.Length;
         }
+
+        public double Average2(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            return (double)sum / numbers.Length;
+        }
     }
 }
--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -5,21 +5,24 @@
         private static void Main(string[] args)
         {
             //Character c = new Character();
-            
+
             //c.Hit(120);
-            
+
             //Console.WriteLine(c.Health);
 
 
-            Calculator calc = new Calculator();
-            //double area1 = calc.GetTriangleArea(2,8);
-            //double area2 = calc.GetTriangleArea(20,21,28);
+            //Calculator calc = new Calculator();
+            //double area1 = calc.GetTriangleArea(2, 8);
+            //double area2 = calc.GetTriangleArea(20, 21, 28);
 
             //Console.WriteLine(area1);
             //Console.WriteLine(area2);
 
-            double avg = calc.Average(new int[] { 1, 2, 3, 4 });
-            Console.WriteLine(avg);
+            //double avg = calc.Average(new int[] { 1, 2, 3, 4 });
+            //Console.WriteLine(avg);
+
+            //double avg2 = calc.Average2(1, 2, 3, 4);
+            //Console.WriteLine(avg2);
 
         }
     }
